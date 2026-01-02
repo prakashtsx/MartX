2ea99856fe544a4a6abc22876a3b2c83409dc9cf
prakashtsx <urprakashgupta@gmail.com>
2026-01-02 21:47:18 +0530

    added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,6 +7,7 @@ import "./assets/styles/index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
 import HomeScreen from "./screens/HomeScreen.jsx";
+import ProductScreen from "./screens/ProductScreen.jsx";
 
 const router = createBrowserRouter([
   {
@@ -16,6 +17,10 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <HomeScreen />,
+      },
+      {
+        path: "/product/:id",
+        element: <ProductScreen />,
       },
     ],
   },
