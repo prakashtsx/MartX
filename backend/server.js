d78d2ad3d05bd7e9eaac948743e81db149a2bf32
prakashtsx <urprakashgupta@gmail.com>
2026-01-03 01:21:59 +0530

    feat: implement product fetching from API, update routes, and add dotenv support

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,31 @@
 import express from 'express';
 import products from './data/products.js'
-const PORT = 5000;
+import dotenv from 'dotenv';
+
+const PORT = process.env.PORT || 5000;
+
+dotenv.config();
 
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("hello world")
+    res.send("good to go")
 })
 
 //fetch data from product
-app.get('/products', (req, res) => {
+app.get('/api/products', (req, res) => {
     res.send(products);
 });
 
+
 //fetch data from product id
-app.get('/products/:id', (req, res) => {
+app.get('/api/products/:id', (req, res) => {
     const product = products.find((p) => p._id === req.params.id);
     res.send(product);
 
 })
 
 app.listen(PORT, () => {
-    console.log("server is runnign on port", PORT);
+    console.log("server is running on port", PORT);
 
 })
