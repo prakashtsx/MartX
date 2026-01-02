74abd717c4a8d713c567f2394e8880f319381331
prakashtsx <urprakashgupta@gmail.com>
2026-01-03 01:39:34 +0530

    feat: add MongoDB connection setup and mongoose dependency

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import products from './data/products.js'
 import dotenv from 'dotenv';
+import connectDB from './config/db.js';
 
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
+connectDB();
 
 const app = express();
 
